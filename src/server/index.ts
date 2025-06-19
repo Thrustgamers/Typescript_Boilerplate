ec0019a0007786a507a262ffbe94b0d51aaf0c26
Thrustgamers <94409067+Thrustgamers@users.noreply.github.com>
2025-06-19 23:36:52 +0200

    initial commit

---
+++ src/server/index.ts
@@ -0,0 +1,10 @@
+// import { onClientCallback } from '@communityox/ox_lib/server';
+// import { oxmysql } from '@communityox/oxmysql';
+
+
+// onClientCallback('typescript_boilerplate:test', async (): Promise<any> => {
+
+//   return {
+//     status: "cool right?"
+//   };
+// });
